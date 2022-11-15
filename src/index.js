1dafb8f3b71c7c21194c9985e27f2ad8ed3d909a
Hoós Roland <shadeeygg@gmail.com>
2022-11-15 15:18:16 +0100

    Feladat Befejzése

--- src/index.js
+++ src/index.js
@@ -1,2 +1,106 @@
 import './style.css';
 
+var tomb = [];
+var hossz = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('mindGomb').addEventListener('click', async() => {
+        let response = await fetch('./quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes;
+        idezetek = idezetek.sort((a, b) => {
+            if (a.author < b.author) {
+              return -1;
+            }
+          });
+        adatMegjelenites(idezetek);
+        
+    });
+
+    document.getElementById('theGomb').addEventListener('click', async() => {
+        let response = await fetch('./quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes;
+        theMegjelenites(idezetek);
+        
+    });  
+    
+    document.getElementById('hosszGomb').addEventListener('click', async() => {
+        let response = await fetch('./quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes;
+        hosszMegjelenites(idezetek);
+        
+    });  
+
+    document.getElementById('dbGomb').addEventListener('click', async() => {
+        let response = await fetch('./quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes;
+        darabMegjelenites(idezetek);
+        
+    });  
+})
+
+
+
+function adatMegjelenites(idezetek) {
+    let lista = document.getElementById('szerzok');
+    lista.innerHTML = "";
+    for (let p of idezetek) {
+        let li = document.createElement('li');
+        li.textContent = p.author;
+        lista.appendChild(li);
+    }
+}
+
+function theMegjelenites(idezetek){
+    tomb = [];
+    let lista = document.getElementById('theLista');
+    lista.innerHTML = "";
+    var regexThe = /(?:^|\W)the(?:$|\W)/;
+    var regexNagyThe = /(?:^|\W)The(?:$|\W)/;
+
+    for (let p of idezetek) {
+        let adat = p.quote;
+        tomb.push(adat);
+    }
+
+    for (let i = 0; i<tomb.length;i++) {
+        let li = document.createElement('li');
+        li.innerHTML = tomb[i].replace(regexThe, '<b> the </b>').replace(regexNagyThe, '<b> The </b>');
+        lista.appendChild(li);
+    }
+}
+
+function hosszMegjelenites(idezetek){
+    hossz = [];
+    let lista = document.getElementById('hosszEredmeny');
+    lista.innerHTML = "";
+
+    for (let p of idezetek) {
+        let adat = p.quote.length;
+        hossz.push(adat);
+    }
+
+    let szoveg = hossz.join(", ")
+    lista.innerHTML=szoveg
+}
+
+function darabMegjelenites(idezetek){
+    let szerzo = document.getElementById('szerzoText').value;
+    console.log(szerzo)
+    
+    let darabSzam = 0;
+
+    let idezet = idezetek.filter(function(idezetek){
+        return idezetek.author == szerzo;
+    })
+
+    for (let p of idezet) {
+        darabSzam++;
+    }
+
+    document.getElementById('dbSzam').value = darabSzam;
+    
+}
